06ceba3f9c5716c0155ccf0580390458462b76ed
marioczarco <31322191+marioczarco@users.noreply.github.com>
2019-06-04 19:44:25 -0700

    booked_flights.sql
    This is for the database of tables used for our AirplaneFlightScheduler

---
+++ booked_flights.sql
@@ -0,0 +1,27 @@
+CREATE SCHEMA booked_flights;
+CREATE TABLE airplane(
+airplanes VARCHAR(33) DEFAULT NULL,
+goTime TIME DEFAULT NULL,
+helloTime TIME DEFAULT NULL,
+destination VARCHAR(33) DEFAULT NULL
+);
+CREATE TABLE confirmedseats(
+seats VARCHAR(2) DEFAULT NULL,
+confirmationNum CHAR(8) DEFAULT NULL
+);
+CREATE TABLE destination(
+poi VARCHAR(33) DEFAULT NULL,
+timefromAtoB VARCHAR(33) DEFAULT NULL,
+onetimefromAtoB VARCHAR(33) DEFAULT NULL
+);
+CREATE TABLE reserved(
+passenger_name VARCHAR(33) DEFAULT NULL,
+destination VARCHAR(33) DEFAULT NULL,
+departureTime TIME DEFAULT NULL,
+arrivalTime TIME DEFAULT NULL,
+bkmealplan VARCHAR(33) DEFAULT NULL,
+lunchmealplan VARCHAR(33) DEFAULT NULL,
+dinnermealplan VARCHAR(33) DEFAULT NULL,
+confirmation CHAR(8) DEFAULT NULL,
+luggage tinyint(4) DEFAULT NULL
+);
