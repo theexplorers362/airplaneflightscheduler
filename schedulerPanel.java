d827eb2ce8df25f2412b5696c93945b2132ee5f7
marioczarco <31322191+marioczarco@users.noreply.github.com>
2019-06-20 14:28:04 -0700

    Takes care of function implementation Update schedulerPanel.java

--- schedulerPanel.java
+++ schedulerPanel.java
@@ -10,9 +10,9 @@ import javax.swing.*;
 import java.sql.*;
 
 public class schedulerPanel extends JPanel {
-JPanel northernPanel;
 public schedulerPanel() {
 	setLayout(new BorderLayout());
+	String confirmation;
 	westernPanels neww = new westernPanels();
 	centerPanels newc = new centerPanels();
 	easternPanels newe = new easternPanels();
@@ -23,6 +23,22 @@ public schedulerPanel() {
 	this.add(neww.getTextArea(), BorderLayout.WEST);
 	this.add(newc.getOptionsArea(),BorderLayout.CENTER);
 	this.add(newe.getButtonArea(), FlowLayout.RIGHT);
-
+	neww.togglethemButtons();
+	neww.addActionListenerHelperComboBennie();
+	newe.addActionListenerHelperbkButtons(newc.bkButton1, newc.bkgroup);
+	newe.addActionListenerHelperbkButtons(newc.bkButton2, newc.bkgroup);
+	newe.addActionListenerHelperbkButtons(newc.bkButton3, newc.bkgroup);
+	newe.addActionListenerHelperbkButtons(newc.bkButton4, newc.bkgroup);
+	newe.addActionListenerHelperLunchButtons(newc.lunchButton1, newc.lunchgroup);
+	newe.addActionListenerHelperLunchButtons(newc.lunchButton2, newc.lunchgroup);
+	newe.addActionListenerHelperLunchButtons(newc.lunchButton3, newc.lunchgroup);
+	newe.addActionListenerHelperLunchButtons(newc.lunchButton4, newc.lunchgroup);
+    	newe.addActionListenerHelperDinnerButtons(newc.dinnerButton1, newc.dinnergroup);
+    	newe.addActionListenerHelperDinnerButtons(newc.dinnerButton2, newc.dinnergroup);
+    	newe.addActionListenerHelperDinnerButtons(newc.dinnerButton3, newc.dinnergroup);
+    	newe.addActionListenerHelperDinnerButtons(newc.dinnerButton4, newc.dinnergroup);
+	newe.stateofButtons();
+    	newe.pressConfirm(neww.confirmButton);
+	neww.pressConfirm();
 }
 }
