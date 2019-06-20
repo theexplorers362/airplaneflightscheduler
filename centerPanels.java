3a9cb48904933942e0c0f7cc601a6be7c3856bdd
marioczarco <31322191+marioczarco@users.noreply.github.com>
2019-06-20 14:31:27 -0700

    arranges mealPlan buttons Update centerPanels.java

--- centerPanels.java
+++ centerPanels.java
@@ -5,69 +5,47 @@ import javax.swing.*;
 import java.sql.*;
 
 public class centerPanels {
-	
+	centerPanels(){
+		bkgroup = new ButtonGroup();
+		lunchgroup = new ButtonGroup();
+		dinnergroup = new ButtonGroup();
+	}
 	JPanel buttonsPanel;
 	JLabel bkFastLabel;
-	JButton bkButton1;
-	JButton bkButton2;
-	JButton bkButton3;
-	JButton bkButton4;
+	JRadioButton bkButton1;
+	JRadioButton bkButton2;
+	JRadioButton bkButton3;
+	JRadioButton bkButton4;
 	JLabel lunchLabel;
-	JButton lunchButton1;
-	JButton lunchButton2;
-	JButton lunchButton3;
-	JButton lunchButton4;
+	JRadioButton lunchButton1;
+	JRadioButton lunchButton2;
+	JRadioButton lunchButton3;
+	JRadioButton lunchButton4;
 	JLabel dinnerLabel;
-	JButton dinnerButton1;
-	JButton dinnerButton2;
-	JButton dinnerButton3;
-	JButton dinnerButton4;
+	JRadioButton dinnerButton1;
+	JRadioButton dinnerButton2;
+	JRadioButton dinnerButton3;
+	JRadioButton dinnerButton4;
 	Reservation newReservation;
-	
-	public void addActionListenerHelperbkButtons(JButton any) {
-	any.addActionListener(new ActionListener() { 
-		public void actionPerformed(ActionEvent arg0) {
-			try {
-				newReservation.setBkfastoption(any.getText());
-				
-				
-			}catch(Exception e){
-				e.printStackTrace();
-			}
-			
-		}
-	});
-	}
+	ButtonGroup bkgroup;
+	ButtonGroup lunchgroup;
+	ButtonGroup dinnergroup;
+	 /*public void getTimeSpans()
+	 {
+	     boolean firstTime = false, secondTime = false;
 
-	public void addActionListenerHelperLunchButtons(JButton any) {
-		any.addActionListener(new ActionListener() { 
-			public void actionPerformed(ActionEvent arg0) {
-				try {
-					newReservation.setLunchoption(any.getText());
-					
-					
-				}catch(Exception e){
-					e.printStackTrace();
-				}
-				
-			}
-	});
-	}
-	
-	public void addActionListenerHelperDinnerButtons(JButton any) {
-		any.addActionListener(new ActionListener() { 
-			public void actionPerformed(ActionEvent arg0) {
-				try {
-					newReservation.setDinneroption(any.getText());
-					
-					
-				}catch(Exception e){
-					e.printStackTrace();
-				}
-				
-			}
-	});
-	}
+	     if(time1 > "20:11:13" && time1 < "14:49:00")
+	     {
+	        firstTime = true;
+	     }
+
+	     if(time2 > "20:11:13" && time2 < "14:49:00")
+	     {
+	        secondTime = true;
+	     }
+	  }*/
+	 
+
 	
 	/*Boundary Component of class centerPanels
 	 * 
@@ -80,73 +58,64 @@ public Component getOptionsArea() {
 	buttonsPanel = new JPanel();
 	buttonsPanel.setLayout(new FlowLayout());
 	bkFastLabel = new JLabel("BreakFast: ");
-	bkButton1 = new JButton("Omelette");
-	bkButton2 = new JButton("Waffles");
-	bkButton3 = new JButton("Coffee Cake");
-	bkButton4 = new JButton("Acai Bowl");
+	bkButton1 = new JRadioButton("Omelette");
+	bkButton2 = new JRadioButton("Waffles");
+	bkButton3 = new JRadioButton("Coffee Cake");
+	bkButton4 = new JRadioButton("Acai Bowl");
 	lunchLabel = new JLabel("Lunch: ");
-	lunchButton1 = new JButton("BLT Sandwich");
-	lunchButton2 = new JButton("Pizza");
-	lunchButton3 = new JButton("Pasta Alfredo");
-	lunchButton4 = new JButton("Soup");
+	lunchButton1 = new JRadioButton("BLT Sandwich");
+	lunchButton2 = new JRadioButton("Pizza");
+	lunchButton3 = new JRadioButton("Pasta Alfredo");
+	lunchButton4 = new JRadioButton("Soup");
 	dinnerLabel = new JLabel("Dinner: ");
-	dinnerButton1 = new JButton("Steak");
-	dinnerButton2 = new JButton("Lobster");
-	dinnerButton3 = new JButton("Shrimp Cocktail");
-	dinnerButton4 = new JButton("Caesar Salad");
+	dinnerButton1 = new JRadioButton("Steak");
+	dinnerButton2 = new JRadioButton("Lobster");
+	dinnerButton3 = new JRadioButton("Shrimp Cocktail");
+	dinnerButton4 = new JRadioButton("Caesar Salad");
+	buttonsPanel.add(Box.createRigidArea(new Dimension(7, 0)));
 	buttonsPanel.add(bkFastLabel);
-	ButtonGroup bkgroup = new ButtonGroup();
 	bkgroup.add(bkButton1);
 	bkgroup.add(bkButton2);
 	bkgroup.add(bkButton3);
 	bkgroup.add(bkButton4);
-	addActionListenerHelperbkButtons(bkButton1);
-	addActionListenerHelperbkButtons(bkButton2);
-	addActionListenerHelperbkButtons(bkButton3);
-	addActionListenerHelperbkButtons(bkButton4);
 	
 	
+	buttonsPanel.add(Box.createRigidArea(new Dimension(15, 0)));
 	buttonsPanel.add(bkButton1, BorderLayout.CENTER);
+	buttonsPanel.add(Box.createRigidArea(new Dimension(30, 0)));
 	buttonsPanel.add(bkButton2, BorderLayout.CENTER);
-	buttonsPanel.add(Box.createRigidArea(new Dimension(93, 0)));
+	buttonsPanel.add(Box.createRigidArea(new Dimension(108, 0)));
 	buttonsPanel.add(bkButton3, BorderLayout.CENTER);
+	buttonsPanel.add(Box.createRigidArea(new Dimension(13, 0)));
 	buttonsPanel.add(bkButton4, BorderLayout.CENTER);
-	buttonsPanel.add(Box.createRigidArea(new Dimension(10, 0)));
 	buttonsPanel.add(lunchLabel);
-	ButtonGroup lunchgroup = new ButtonGroup();
 	lunchgroup.add(lunchButton1);
 	lunchgroup.add(lunchButton2);
 	lunchgroup.add(lunchButton3);
 	lunchgroup.add(lunchButton4);
-	addActionListenerHelperLunchButtons(lunchButton1);
-	addActionListenerHelperLunchButtons(lunchButton2);
-	addActionListenerHelperLunchButtons(lunchButton3);
-	addActionListenerHelperLunchButtons(lunchButton4);
 	
 	
-	buttonsPanel.add(Box.createRigidArea(new Dimension(20, 0)));
+	buttonsPanel.add(Box.createRigidArea(new Dimension(32, 0)));
 	buttonsPanel.add(lunchButton1, BorderLayout.CENTER);
+	buttonsPanel.add(Box.createRigidArea(new Dimension(2, 0)));
 	buttonsPanel.add(lunchButton2, BorderLayout.CENTER);
-	buttonsPanel.add(Box.createRigidArea(new Dimension(82, 0)));
+	buttonsPanel.add(Box.createRigidArea(new Dimension(110, 0)));
 	buttonsPanel.add(lunchButton3, BorderLayout.CENTER);
+	buttonsPanel.add(Box.createRigidArea(new Dimension(6, 0)));
 	buttonsPanel.add(lunchButton4, BorderLayout.CENTER);
-	buttonsPanel.add(Box.createRigidArea(new Dimension(50, 0)));
+	buttonsPanel.add(Box.createRigidArea(new Dimension(30, 0)));
 	buttonsPanel.add(dinnerLabel);
-	ButtonGroup dinnergroup = new ButtonGroup();
 	dinnergroup.add(dinnerButton1);
 	dinnergroup.add(dinnerButton2);
 	dinnergroup.add(dinnerButton3);
 	dinnergroup.add(dinnerButton4);
-	addActionListenerHelperDinnerButtons(dinnerButton1);
-	addActionListenerHelperDinnerButtons(dinnerButton2);
-	addActionListenerHelperDinnerButtons(dinnerButton3);
-	addActionListenerHelperDinnerButtons(dinnerButton4);
 	
 	
-	buttonsPanel.add(Box.createRigidArea(new Dimension(25, 0)));
+	buttonsPanel.add(Box.createRigidArea(new Dimension(35, 0)));
 	buttonsPanel.add(dinnerButton1, BorderLayout.CENTER);
+	buttonsPanel.add(Box.createRigidArea(new Dimension(51, 0)));
 	buttonsPanel.add(dinnerButton2, BorderLayout.CENTER);
-	buttonsPanel.add(Box.createRigidArea(new Dimension(75, 0)));
+	buttonsPanel.add(Box.createRigidArea(new Dimension(97, 0)));
 	buttonsPanel.add(dinnerButton3, BorderLayout.CENTER);
 	buttonsPanel.add(dinnerButton4, BorderLayout.CENTER);
 	return buttonsPanel;
