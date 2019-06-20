c8451433346b0159eec9ef5380d7450dc9716254
marioczarco <31322191+marioczarco@users.noreply.github.com>
2019-06-20 14:43:16 -0700

    Update booked_flights.sql

--- booked_flights.sql
+++ booked_flights.sql
@@ -7,21 +7,20 @@ destination VARCHAR(33) DEFAULT NULL
 );
 CREATE TABLE confirmedseats(
 seats VARCHAR(2) DEFAULT NULL,
+bkmealplan VARCHAR(33) DEFAULT NULL,
+lunchmealplan VARCHAR(33) DEFAULT NULL,
+dinnermealplan VARCHAR(33) DEFAULT NULL,
 confirmationNum CHAR(8) DEFAULT NULL
 );
 CREATE TABLE destination(
 poi VARCHAR(33) DEFAULT NULL,
-timefromAtoB VARCHAR(33) DEFAULT NULL,
-onetimefromAtoB VARCHAR(33) DEFAULT NULL
+nonstopfromAtoB VARCHAR(33) DEFAULT NULL
 );
 CREATE TABLE reserved(
 passenger_name VARCHAR(33) DEFAULT NULL,
 destination VARCHAR(33) DEFAULT NULL,
 departureTime TIME DEFAULT NULL,
 arrivalTime TIME DEFAULT NULL,
-bkmealplan VARCHAR(33) DEFAULT NULL,
-lunchmealplan VARCHAR(33) DEFAULT NULL,
-dinnermealplan VARCHAR(33) DEFAULT NULL,
 confirmation CHAR(8) DEFAULT NULL,
 luggage tinyint(4) DEFAULT NULL
 );
